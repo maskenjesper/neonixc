f4e7b4645d0527ec8dc2d68a3c65c6b1a2bb4a10
jakob <noreply@email.com>
2025-06-10 16:55:11 +0200

    53 current  2025-06-09 22:12:39  25.11.20250607.3e3afe5     6.12.32                          *

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo "hello world"
+# Automated script to install my nix configuration.
+
+# Parse arguments
+if [ $# -gt 1 ] then
+    HOSTNAME=$1
+    USERNAME=$2
+else
+    echo "Invalid arguments"
+fi
+
+
+echo "Installing nix config..."
+
+echo "Entering nix shell"
+nix-shell -p git
+
+echo "Cloning repo..."
+git clone https://github.com/maskenjesper/neonixc.git ~/neonixc
+
+
