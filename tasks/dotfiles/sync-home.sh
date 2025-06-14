f3f8d3c355acfc6c5e2f7c5d2492733d8e6afb52
jakob <noreply@email.com>
2025-06-14 16:34:52 +0200

    7 current   2025-06-11 23:23:23  25.11.20250607.3e3afe5     6.12.32                          *

---
+++ tasks/dotfiles/sync-home.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo starting
+
+pushd ~/neonixc/ || exit
+
+# nix requires all files in the directory to be either commited or staged
+git add --all
+
+echo ====================== Running home-manager ======================
+home-manager switch --impure --flake . -b backup --show-trace -L -v
+
+if [[ $? -eq 0 ]]; then
+    echo Sync successful 
+else
+    echo Home manager failed. Aborting...
+fi
+
+popd || exit
---
+++ tasks/dotfiles/sync-system.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo echo starting
+
+pushd ~/neonixc/ || exit
+
+# nix requires all files in the directory to be either commited or staged
+git add --all
+
+echo ====================== Running nixos-rebuild ======================
+sudo nixos-rebuild switch --impure --flake . --show-trace -L -v 
+
+if [[ $? -eq 0 ]]; then
+    echo Sync successful 
+else
+    echo Rebuild failed. Aborting...    
+fi
+
+popd || exit
