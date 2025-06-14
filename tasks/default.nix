92cc658ee9600971c2c23aa5f9bd04649c23377f
jakob <noreply@email.com>
2025-06-14 16:23:57 +0200

    7 current   2025-06-11 23:23:23  25.11.20250607.3e3afe5     6.12.32                          *

--- tasks/default.nix
+++ tasks/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   home.file.".config/just" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/jakob/neonixc/tasks/dotfiles";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/neonixc/tasks/dotfiles";
     recursive = true;
   };
 
--- user/desktop-environments/hyprland/submodules/waybar/default.nix
+++ user/desktop-environments/hyprland/submodules/waybar/default.nix
@@ -5,7 +5,7 @@
 }: {
 
   home.file.".config/waybar" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/jakob/neonixc/user/desktop-environments/hyprland/submodules/waybar/dotfiles";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/neonixc/user/desktop-environments/hyprland/submodules/waybar/dotfiles";
     recursive = true;
   };
 }
