4c7e374c7f7f21a8ad22640b73e7aa281f7aaa19
jakob <noreply@email.com>
2025-10-06 20:00:43 +0200

    2025-10-06 18:13:43

--- user/desktop-environments/common/quickshell/default.nix
+++ user/desktop-environments/common/quickshell/default.nix
@@ -5,7 +5,7 @@
 }: {
 
   home.file.".config/quickshell" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/neonixc/user/desktop-environments/hyprland/submodules/quickshell/dotfiles";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/neonixc/user/desktop-environments/common/quickshell/dotfiles";
     recursive = true;
   };
 }
