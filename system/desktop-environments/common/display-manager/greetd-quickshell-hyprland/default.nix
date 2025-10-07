024d43c0f6476aa064c65f3c0dadb578a695e4ef
jakob <noreply@email.com>
2025-10-08 00:56:57 +0200

    2025-10-08 00:56:56

--- system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
+++ system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
@@ -1,28 +1,40 @@
-{ config, lib, pkgs, ... }:
-let
-  swayConfig = pkgs.writeText "greetd-sway-config" ''
-    # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-    bindsym Mod4+shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
-  '';
-in
-{
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
-      };
-    };
-  };
+# { config, lib, pkgs, ... }:
+# let
+#   swayConfig = pkgs.writeText "greetd-sway-config" ''
+#     # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+#     exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+#     bindsym Mod4+shift+e exec swaynag \
+#       -t warning \
+#       -m 'What do you want to do?' \
+#       -b 'Poweroff' 'systemctl poweroff' \
+#       -b 'Reboot' 'systemctl reboot'
+#   '';
+# in
+# {
+#   services.greetd = {
+#     enable = true;
+#     settings = {
+#       default_session = {
+#         command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+#       };
+#     };
+#   };
+#
+#   environment.etc."greetd/environments".text = ''
+#     uwsm start default
+#     fish
+#     bash
+#   '';
+# }
 
-  environment.etc."greetd/environments".text = ''
-    uwsm start default
-    fish
-    bash
-  '';
+{ config, lib, pkgs, ... }:
+{
+    services.greetd = {
+        enable = true;
+        settings = {
+            default_session = {
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd \"uwsm start default\"";
+            };
+        };
+    };
 }
