3529f6080393f23196b81ab0681c49eefbe7912f
jakob <noreply@email.com>
2025-10-07 23:17:23 +0200

    2025-10-07 23:17:22

--- system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
+++ system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
@@ -1,18 +1,22 @@
 { config, lib, pkgs, ... }:
+let
+  hyprConfig = pkgs.writeText "greetd-hypr-config" ''
+    exec-once = "${pkgs.greetd.qtgreet}/bin/qtgreet; hyprctl dispatch exit"
+  '';
+in
 {
   services.greetd = {
     enable = true;
-    # settings = {
-    #   default_session = {
-    #     command = "${pkgs.hyprland}/bin/hyprland";
-    #   };
-    # };
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland --config ${hyprConfig}";
+      };
+    };
   };
 
   environment.etc."greetd/environments".text = ''
     hyprland
     fish
     bash
-    startxfce4
   '';
 }
