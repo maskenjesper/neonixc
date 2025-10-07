f03cd1c2368dcd1a17c4c22c36349b709ce4ba96
jakob <noreply@email.com>
2025-10-07 23:59:42 +0200

    2025-10-07 23:59:41

--- system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
+++ system/desktop-environments/common/display-manager/greetd-quickshell-hyprland/default.nix
@@ -1,10 +1,39 @@
+# { config, lib, pkgs, ... }:
+# {
+#   services.greetd = {
+#     enable = true;
+#     settings = {
+#       default_session = {
+#         command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet";
+#       };
+#     };
+#   };
+#
+#   environment.etc."greetd/environments".text = ''
+#     Hyprland
+#     fish
+#     bash
+#   '';
+# }
+
 { config, lib, pkgs, ... }:
+let
+  swayConfig = pkgs.writeText "greetd-sway-config" ''
+    # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+    bindsym Mod4+shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+  '';
+in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet";
+        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
       };
     };
   };
