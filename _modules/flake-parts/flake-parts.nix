7ad6fe7eaef4f025e6ee038bcb938aec1cefb4a3
jakob <noreply@email.com>
2025-10-12 11:37:51 +0200

    2025-10-10 23:48:22

--- _modules/flake-parts/flake-parts.nix
+++
@@ -1,5 +0,0 @@
-{ inputs, ... }:
-{
-  # Enables the module system of flake-parts (flake.modules.<class>.<module>)
-  imports = [ inputs.flake-parts.flakeModules.modules ];
-}
--- _user/terminal/apps/nixCats/dotfiles/lua/jakob/core/init.lua
+++
@@ -1,3 +0,0 @@
-require("jakob.core.keymaps")
-require("jakob.core.options")
-require("jakob.core.folding")
