89107b2d862c4190888bec7010868c57b51df3c7
jakob <noreply@email.com>
2025-11-02 22:59:58 +0100

    2025-11-02 22:59:57

--- modules/desktop-environments/hyprland/nixos/default.nix
+++ modules/desktop-environments/hyprland/nixos/default.nix
@@ -23,6 +23,7 @@
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       plugins = [
+        inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprscrolling
       ];
     };
     environment.sessionVariables = {
