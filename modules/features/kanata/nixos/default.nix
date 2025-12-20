56cdc751b5673257447451a493b3feac853aca3a
jakob <noreply@email.com>
2025-12-20 23:54:59 +0100

    2025-12-20 23:54:55

--- modules/features/kanata/nixos/default.nix
+++ modules/features/kanata/nixos/default.nix
@@ -4,12 +4,55 @@
   localUsers,
   ...
 }: {
-  
-  environment.systemPackages = with pkgs; [
-    wtype
-    kanata
-  ];
+  boot.kernelModules = ["uinput"];
+
   hardware.uinput.enable = true;
+
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+
   users.groups.uinput.members = localUsers;
   users.groups.input.members = localUsers;
+
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          # Replace the paths below with the appropriate device paths for your setup.
+          # Use `ls /dev/input/by-path/` to find your keyboard devices.
+          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+           caps tab d h j k l
+          )
+          (defvar
+           tap-time 200
+           hold-time 200
+          )
+          (defalias
+           caps (tap-hold 200 200 esc lctl)
+           tab (tap-hold $tap-time $hold-time tab (layer-toggle arrow))
+           del del  ;; Alias for the true delete key action
+          )
+          (deflayer base
+           @caps @tab d h j k l
+          )
+          (deflayer arrow
+           _ _ @del left down up right
+          )
+        '';
+      };
+    };
+  };
 }
