2448ec99546ce96efc68755b5ca1958240466566
jakob <noreply@email.com>
2025-12-21 00:07:32 +0100

    2025-12-21 00:07:28

--- modules/features/kanata/nixos/default.nix
+++ modules/features/kanata/nixos/default.nix
@@ -37,13 +37,38 @@
            caps tab d h j k l
           )
           (defvar
+           ;; Timing
            tap-time 200
-           hold-time 200
+           hold-time 150
+
+           ;; Keys on same hand that trigger early tap
+           ;; EXCLUDING homerow mod keys so they can combine smoothly
+           left-hand-keys (
+             q w e r t
+             g  ;; excluding a s d f
+             z x c v b
+           )
+
+           right-hand-keys (
+             y u i o p
+             h  ;; excluding j k l ;
+             n m , . /
+           )
           )
           (defalias
            caps (tap-hold 200 200 esc lctl)
            tab (tap-hold $tap-time $hold-time tab (layer-toggle arrow))
            del del  ;; Alias for the true delete key action
+           a (tap-hold-release-keys $tap-time $hold-time a lctl $left-hand-keys)
+           s (tap-hold-release-keys $tap-time $hold-time s lalt $left-hand-keys)
+           d (tap-hold-release-keys $tap-time $hold-time d lmet $left-hand-keys)
+           f (tap-hold-release-keys $tap-time $hold-time f lsft $left-hand-keys)
+
+           ;; Right hand homerow mods (matching pairs)
+           j (tap-hold-release-keys $tap-time $hold-time j rsft $right-hand-keys)
+           k (tap-hold-release-keys $tap-time $hold-time k rmet $right-hand-keys)
+           l (tap-hold-release-keys $tap-time $hold-time l ralt $right-hand-keys)
+           ; (tap-hold-release-keys $tap-time $hold-time ; rctl $right-hand-keys)
           )
           (deflayer base
            @caps @tab d h j k l
